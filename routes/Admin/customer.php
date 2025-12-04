000ec96bb850003d644f22d7ec2799d78342d379
Javed <javedbaloch0091@gmail.com>
2025-12-04 14:25:13 +0500

    updates

--- routes/Admin/customer.php
+++ routes/Admin/customer.php
@@ -3,5 +3,6 @@
 use App\Http\Controllers\Front\CustomerController;
 use Illuminate\Support\Facades\Route;
  
-
+//Customer Module Routes
 Route::get('/customer/details',[CustomerController::class, 'index'])->name('customer.index');
+Route::get('/customer/order/history',[CustomerController::class, 'order_history'])->name('order.history');
