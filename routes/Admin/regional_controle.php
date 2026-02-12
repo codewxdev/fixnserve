83b6af5f036c81d0edaf180f965098e1f010b8f0
Javed Baloch <javedbaloch0091@gmail.com>
2026-02-12 09:42:02 +0500

    updates

--- routes/Admin/regional_controle.php
+++ routes/Admin/regional_controle.php
@@ -2,11 +2,17 @@
 
 use App\Http\Controllers\Front\ProviderController;
 use App\Http\Controllers\Front\RegionalController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
- 
 
 // ====================================================
 // Regional Controle 
 // ====================================================
 Route::get('/regional_controle', [RegionalController::class, 'index'])->name('regional_controle.index');
 
+// routes/api.php
+Route::put('/countries/{id}', function (Request $request, $id) {
+    $country = \App\Models\Country::findOrFail($id);
+    $country->update(['status' => $request->status]);
+    return response()->json(['success' => true]);
+});
