4a61805395c4eccfe4765eede91afe432a3bdfcf
Javed Baloch <javedbaloch0091@gmail.com>
2026-02-24 12:52:18 +0500

    updates

--- database/migrations/2025_12_18_091731_create_currencies_table.php
+++ database/migrations/2025_12_18_091731_create_currencies_table.php
@@ -23,8 +23,5 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
-        Schema::dropIfExists('currencies');
-    }
+    
 };
