f2c29c5fdfbd9558147191ab5c1ca30834434a66
Javed Baloch <javedbaloch0091@gmail.com>
2026-05-05 11:43:10 +0500

    updates

--- app/Http/Controllers/Front/AuthController.php
+++ app/Http/Controllers/Front/AuthController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
    public function index(){
-        return view('Auth.Login');
+        return view('Auth.login');
     }
 
     public function forget(){
