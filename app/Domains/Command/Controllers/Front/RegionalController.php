7966cc4b41379c147c579d5fac2a91eea525ef8c
Javed Baloch <javedbaloch0091@gmail.com>
2026-05-05 16:36:55 +0500

    updates

--- app/Domains/Command/Controllers/Front/RegionalController.php
+++ app/Domains/Command/Controllers/Front/RegionalController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class RegionalController extends Controller
 {
     public function index(){
-        return view('Admin.Command.regions.index');
+        return view('Admin.Command.Regions.index');
     }
 
     public function updateStatus(Request $request, $id)
